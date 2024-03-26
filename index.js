8188045455c3f790a6d27efbf4041e3b41bcaf38
garvit <garvitchutani07@gmail.com>
2024-03-27 01:29:57 +0530

    feat:created route and controller for login and signup

---
+++ index.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const mongoose=require('mongoose');
+
+const userRouter=require("./routes/userRoutes");
+
+const app=express();
+const port= 3001;
+var cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use("/", userRouter);
+
+app.get('/',(req,res)=>{
+    res.send("hello");
+});
+
+mongoose.connect("mongodb+srv://garvitchutani11062001:r1WSr2pbq9j8Ljwg@cluster0.korxp4a.mongodb.net/")
+.then(()=>{
+    app.listen(port, () => {
+        console.log(`Server is listening at http://localhost:${port}`);
+    });
+})
+.catch((error)=>{
+    console.log(error);
+})
+
+
